e3e2b2404d75f9933abd721f80674647e9a9794a
Ilia Vlasov <elijah.vlasov@gmail.com>
2025-02-24 17:13:32 +0000

    Set up rescript

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,9 +5,10 @@ then
     export HOME=/root/
     . /.bash_env
     bundle config set --local path '/.gem' && 
+        npm run build &&
+        npm run res:build &&
         bundle install &&
         bundle exec jekyll build &&
-        npm run build &&
         typst compile cv/resume_yaml.typ _site/cv.pdf
 else 
     bash 
